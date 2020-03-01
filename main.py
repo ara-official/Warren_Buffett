2f223b873bf112be99152ed7327a6bc4e098680d
jaehunchoi <dydrms44@naver.com>
2020-03-01 18:48:42 +0900

    temp

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from stock.creon.util import creon_99_algorithm
 from stock.creon.util import utils
 from Data_Process import json_utils
 if __name__ == '__main__':
-    json_utils.json_write('minsik.json', 'hi', 'bye')
-    print(json_utils.json_read('minsik.json', 'hi',))
+
     # creon_99_algorithm.Algorithm().algorithm_3__stock_purchase_recommandation(전체기간=20, marketType=1, top=1, bPrint=True)
     creon_99_algorithm.Algorithm().algorithm_4()
     exit()
