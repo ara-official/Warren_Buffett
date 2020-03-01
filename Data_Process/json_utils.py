8fe990eefac98d5c24740de7499ca437890ff265
jaehunchoi <dydrms44@naver.com>
2020-03-01 19:01:03 +0900

    revision of json func

--- Data_Process/json_utils.py
+++ Data_Process/json_utils.py
@@ -1,17 +1,23 @@
 import json
 from collections import OrderedDict
-
+import os
+import sys
 
 def json_read(json_file_name, key):
+    reletive_path=os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+    json_file_name=str(reletive_path)+'\\json\\'+json_file_name
     print("json file " + json_file_name)
-    json_file_name="../json/"+json_file_name
+    
     with open(json_file_name, "r") as json_file:
         json_data = json.load(json_file)
     return json_data[key]
 
 def json_write(json_file_name, key, value=""):
-    print("json file " + os.path.dirname( os.path.abspath( __file__ ) )+json_file_name)
-    json_file_name="War/json/"+json_file_name
+    
+    reletive_path=os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+    json_file_name=str(reletive_path)+'\\json\\'+json_file_name
+    print("json file " + json_file_name)
+    
     file_data = dict()
     file_data[key] = value
     json.dumps(file_data, ensure_ascii=False, indent="\t")
