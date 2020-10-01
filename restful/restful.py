713c6d6e2623021c785d2ab2fe528f41cfb81133
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2020-10-02 01:28:46 +0900

    add lib for changing string to dictionary

--- restful/restful.py
+++ restful/restful.py
@@ -1,5 +1,8 @@
 import requests
+from ast import literal_eval # string to dictionray
+
 import crtfc_key
+
 
 API_HOST = "https://opendart.fss.or.kr/api/company.json"
 CORP_CODE = "00126380"
@@ -12,6 +15,9 @@ def get_company_information(corp_code):
 
 res = get_company_information(CORP_CODE)
 
-print(res.status_code)
-print(res.headers)
-print(res.text)
+print("[1] ", res.status_code)
+print("[2] ", res.headers)
+print("[3] ", res.text)
+t = literal_eval(res.text)
+print("[4] ", t["status"])
+print("[4] ", t["stock_name"])
